10ba6fea3144bee1a190cf5388aa9825dce0e457
M.Niranjan <niranjan20267@gmail.com>
2026-03-08 20:24:55 +0530

    Implement room joining functionality in Home component

--- room.page.tsx
+++ room.page.tsx
@@ -1,1 +1,16 @@
+"use client";   
+import styles from "./page.module.css";
+import{ useRef, useState } from "react";
+import{useRouter} from "next/navigation";
+export default function Home() {
+const roomId = useRef<HTMLInputElement>(null);
+const router = useRouter();
+  return (
 
+    <div className={styles.main}>
+      <input type="text" className={styles.input} placeholder="Enter meeting id" ref={roomId} />
+      <button className={styles.button} type="submit" onClick={()=> router.push( `/room/${roomId.current?.value}`)}>Join</button> 
+    </div>
+    
+  );
+}
